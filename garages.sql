aa8aa8de9f73383c55cb6c85ebc10310c38c7945
Elliot Rodriguez <Elliot Rodriguez>
2017-10-30 23:15:28 -0400

    fixed sql

--- garages.sql
+++ garages.sql
@@ -29,35 +29,3 @@ values ('I', 4, 1231, 999);
 
 insert into garages
 values ('Libra', 6, 1007, 454);
-
-
-
-insert into garage_st
-values ('A', 0);
-
-insert into garage_st
-values ('B', 0);
-
-insert into garage_st
-values ('C', 0);
-
-insert into garage_st
-values ('D', 0);
-
-insert into garage_st
-values ('E',0);
-
-insert into garage_st
-values ('F', 0);
-
-insert into garage_st
-values ('G', 0);
-
-insert into garage_st
-values ('H',0);
-
-insert into garage_st
-values ('I',0);
-
-insert into garage_st
-values ('Libra', 0);
